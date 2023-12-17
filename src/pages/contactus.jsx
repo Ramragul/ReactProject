203d0aba38364d58111444627690f6f6ecb0c481
Ramragul <iotprograms@gmail.com>
2023-12-17 20:14:11 +0400

    Modifying contact us submit button for testing

--- src/pages/contactus.jsx
+++ src/pages/contactus.jsx
@@ -100,7 +100,7 @@ export const ContactUs = () => {
             {/* <Button variant="gradient" size="lg" className="mt-8" type="submit">
               Send Message
             </Button> */}
-            <Button onClick={() => alert("Clicked")}>
+            <Button onClick={() => console.log("Clicked")}>
             POSTNAVI
             </Button>
             
