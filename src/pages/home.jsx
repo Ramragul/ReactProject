b5fb8565c1e552e950c53cd54a25ff4541c892d5
Ramragul <iotprograms@gmail.com>
2024-04-21 14:22:59 +0400

    Plan Additions

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -110,8 +110,8 @@ export function Home() {
                   <br/>
                   <br/>
                   <Typography
-                    variant="h5"
-                    color="#880E4F"
+                    variant="h6"
+                    color="880E4F"
                     className="mb-3 font-bold"
                   >
                   1. Budget Package  - 10000 rs
